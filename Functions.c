8d1d157e53be0278a121d779cb285ead10fec7d9
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-04-04 14:59:48 +0100

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void birthday(){
+
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear...YOU!");
+}
+
+int main(){
+
+    birthday();
+    birthday();
+    
+    return 0;
+}
